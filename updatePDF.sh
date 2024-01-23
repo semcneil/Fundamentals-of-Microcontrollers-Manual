01762bb0f8efe8199c2516f5ba08fa22320f1be8
Seth McNeill <seth.mcneill@erau.edu>
2024-01-23 09:17:09 -0700

    Updated wait times in updatePDF

--- updatePDF.sh
+++ updatePDF.sh
@@ -12,10 +12,10 @@ git merge dev
 git push origin main
 git checkout docs
 git merge main
-sleep 2
+sleep 5
 echo "make clean to remove old files"
 make clean
-sleep 2
+sleep 7
 echo "Making new PDF"
 make
 git status
